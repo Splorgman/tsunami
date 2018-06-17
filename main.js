2fbd2ed591b23fbf812a5d8d19b7e069e9a7a14f
Simon Westlake <simon@sonar.software>
2018-06-17 11:41:02 -0500

    Move offset calc back into the main process until I can figure out how to make the sub process work

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ var ffbinaries = require('ffbinaries');
 var nodeid3 = require('node-id3');
 var bluebird = require('bluebird');
 var spawn = require('child_process').spawn;
+var AudioContext = require('web-audio-api').AudioContext
+var context = new AudioContext
+var nodeid3 = require('node-id3');
 
 const { exec } = require('child_process');
 const {app, BrowserWindow, ipcMain} = require('electron')
@@ -128,18 +131,141 @@ function adjustTempo(file, bpm, directory, event, position) {
 
 function rewriteOffset(file, event, position) {
   event.sender.send('tempo-reply', { position: position, status: 'offset-start' });
+  var tags = {
+    "COMM": {
+      "language": "eng",
+      "text": 'PENDING OFFSET REWRITE'
+    }
+  }
+
+  var success = nodeid3.update(tags, file);
+  calculateAndWriteOffset(file, event, position);
+}
+
+function calculateAndWriteOffset(mp3, event, position) {
   if (runningOffsetProcesses < 2) {
-    console.log("Calling offset.js with " + file);
-    var process = spawn('node', ['./offset.js ', file]);
-    process.on('exit', function() {
-      event.sender.send('tempo-reply', { position: position, status: 'offset-complete' });
-    })
+    runningOffsetProcesses += 1;
+    console.log("Decoding " + mp3)
+    fs.readFile(mp3, function(err, buf) {
+      if (err) throw err
+      context.decodeAudioData(buf, function(audioBuffer) {
+        console.log("Channels: " + audioBuffer.numberOfChannels);
+        console.log("Frame Length: " + audioBuffer.length);
+        console.log("Sample Rate: " + audioBuffer.sampleRate);
+        console.log("Duration (Seconds): " + audioBuffer.duration);
+        var pcmData = (audioBuffer.getChannelData(0));
+        var start = findStart(pcmData, audioBuffer);
+        console.log(start);
+        var tags = {
+          "COMM": {
+            "language": "eng",
+            "text": '{"Offset": ' + start + '}'
+          }
+        }
+
+        console.log("Setting offset to " + start + " on " + mp3);
+        var success = nodeid3.update(tags, mp3);
+        console.log("Offset write complete for " + mp3);
+        event.sender.send('tempo-reply', { position: position, status: 'offset-complete' });
+
+        if (success !== true) {
+          console.log('\x1b[31m',"Failed to update " + mp3);
+          runningOffsetProcesses -= 1; 
+          return false;
+        }
+        else {
+          runningOffsetProcesses -= 1; 
+          return true;
+        }
+      }, function(err) { 
+        console.log("Error when writing offset for " + mp3);
+        console.log(err);
+        event.sender.send('tempo-reply', { position: position, status: 'offset-complete' });
+        runningOffsetProcesses -= 1; 
+        throw err 
+      });
+    });
   } else {
     console.log("Queueing offset calculation");
     setTimeout(function() {
-      rewriteOffset(file, event, position);
+      calculateAndWriteOffset(mp3, event, position);
     }, 5000);
   }
+  
+}
+
+function findStart(pcmData, audioBuffer) {
+  var levels = getLevels(pcmData, audioBuffer, 10);
+
+  var firstNoiseTime = 0;
+  var firstNoiseLevel = 0;
+
+  var peaks = [];
+  var previousVolume = 0;
+  var threshold = 0.3;
+
+  for (var i = 0; i < levels.length; i++) {
+    if (firstNoiseTime === 0 && levels[i].value > 0.01) {
+      firstNoiseTime = levels[i].time;
+      firstNoiseLevel = levels[i].value;
+    }
+
+    var currentVolume = levels[i].value;
+    if (currentVolume-previousVolume >= threshold) {
+      peaks.push({
+        "time": levels[i].time,
+        "level": currentVolume
+      })
+    }
+    previousVolume = currentVolume;
+  }
+
+  if (peaks[0].time - firstNoiseTime > 5000) {
+      var start = firstNoiseTime;
+  } else {
+    var start = peaks[0].time;
+  }
+
+  return start;
+}
+
+function getLevels(pcmData, audioBuffer, interval) {
+  var step = Math.round((audioBuffer.sampleRate/1000) * interval);
+
+  var levels = [];
+
+  var stepCounter = 0;
+  var previousVolume = 0;
+  var time = 0;
+
+  var i = 0;
+  while (i <= audioBuffer.length) {
+    while (stepCounter <= step) {
+      if (stepCounter > audioBuffer.length) {
+        break;
+      }
+
+      if (pcmData[i] !== undefined) {
+        var volume = pcmData[i].toFixed(5);
+
+        if (volume > previousVolume) {
+          previousVolume = volume;
+          time = Math.round((levels.length * interval) + ((10/step)*stepCounter));
+        }
+      }
+      stepCounter++;
+      i++;
+    }
+
+    levels.push({
+      "time": time,
+      "value": previousVolume
+    });
+
+    previousVolume = 0;
+    stepCounter = 0;
+  }
+  return levels;
 }
 
 function downloadFfBinaries() {
